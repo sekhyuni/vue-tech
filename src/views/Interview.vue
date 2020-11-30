5fa17b9c2737e03eea8495d5cf504d52d873c36a
김석현 <sekhyuni@gmail.com>
2020-11-30 20:28:47 +0900

    Interview.vue Component 추가

---
+++ src/views/Interview.vue
@@ -0,0 +1,32 @@
+<template>
+  <section class="interview">
+    <header>
+      <h1>This is an interview page</h1>
+      <img alt="Naver logo" src="../assets/naver.png">
+    </header>
+
+    <article>
+      <Naver msg="Welcome to Infomation of Naver"/>
+    </article>
+  </section>
+</template>
+
+<script>
+// @ is an alias to /src
+import Naver from '@/components/Naver.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Naver
+  }
+}
+</script>
+
+<style scoped lang="scss">
+$base-color: #00FF00;
+
+h1 {
+  color: desaturate($base-color, 40%);
+}
+</style>
